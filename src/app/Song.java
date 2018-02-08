fa8d57b17058ee54503fa893e26d74b6d816b6cc
inamena <32109389+inamena@users.noreply.github.com>
2018-02-08 18:58:44 -0500

    Add files via upload

---
+++ src/app/Song.java
@@ -0,0 +1,20 @@
+package app;
+
+public class Song {
+	public String songName;
+	public String songArtist;
+	public String songYear;
+	public String songAlbum;
+	
+	public Song(String songName, String songArtist) {
+		this.songName = songName;
+		this.songArtist = songArtist;
+	}
+		
+	public Song(String songName, String songArtist, String songYear, String songAlbum) {
+		this.songName = songName;
+		this.songArtist = songArtist;
+		this.songYear = songYear;
+		this.songAlbum = songAlbum;
+	}
+}
