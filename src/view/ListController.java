81c7c317d766fe1aa683b022e86bf8fc52e3b0e0
TL-Yao <31872143+TL-Yao@users.noreply.github.com>
2018-02-09 15:56:28 -0500

    Add files via upload

--- src/view/ListController.java
+++ src/view/ListController.java
@@ -1,9 +1,15 @@
 package view;
 
+import java.util.Optional;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.stage.Stage;
 
 public class ListController {
@@ -30,6 +36,60 @@ public class ListController {
 				"Seahawks",
 				"Steelers",
 				"Jaguars");               
-		listView.setItems(obsList);
+		listView.setItems(obsList); 
+		
+		// select the first item
+		listView.getSelectionModel().select(0);
+		
+		// make listview editable
+		listView.setCellFactory(TextFieldListCell.forListView());  
+		listView.setEditable(true);
+			
+		
+		listView.getItems().add("some new element");
+		
+		// set listener for the items
+		listView
+		.getSelectionModel()
+		.selectedIndexProperty()
+		.addListener(
+				(obs, oldVal, newVal) -> 
+				//showItem(mainStage)
+				showItemInputDialog(mainStage)
+				);
+				
 	}
+	
+	public void showItem(Stage mainStage) {                
+		Alert alert = 
+				new Alert(AlertType.INFORMATION);
+		alert.initOwner(mainStage);
+		alert.setTitle("List Item");
+		alert.setHeaderText(
+				"Selected list item properties");
+
+		String content = "Index: " + 
+				listView.getSelectionModel()
+		.getSelectedIndex() + 
+		"\nValue: " + 
+		listView.getSelectionModel()
+		.getSelectedItem();
+
+		alert.setContentText(content);
+		alert.show();
+
+	}
+
+	public void showItemInputDialog(Stage mainStage) {                
+		String item = listView.getSelectionModel().getSelectedItem();
+		int index = listView.getSelectionModel().getSelectedIndex();
+
+		TextInputDialog dialog = new TextInputDialog(item);
+		dialog.initOwner(mainStage); dialog.setTitle("List Item");
+		dialog.setHeaderText("Selected Item (Index: " + index + ")");
+		dialog.setContentText("Enter name: ");
+
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()) { obsList.set(index, result.get()); }
+	}	
 }
